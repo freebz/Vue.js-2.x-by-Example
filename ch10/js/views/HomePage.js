5ebacb795a8d97c3d5e56bcad5cc03f4aaa9920e
FreeBz <freebz@hananet.net>
2021-06-08 19:21:49 +0900

    Building an E-Commerce Store - Browsing Products

---
+++ ch10/js/views/HomePage.js
@@ -0,0 +1,35 @@
+const HomePage = {
+  name: 'HomePage',
+
+  template: `<div>
+    <list-products :products="products"></list-products>
+  </div>`,
+
+  data() {
+    return {
+      selectedProducts: [
+	'adjustable-stem',
+	'colorful-fixie-lima',
+	'fizik-saddle-pak',
+	'kenda-tube',
+	'oury-grip-set',
+	'pure-fix-pedals-with-cages'
+      ]
+    }
+  },
+  
+  computed: {
+    products() {
+      let products = this.$store.state.products,
+	  output = [];
+
+      console.log(products);
+      if(Object.keys(products).length) {
+	for(let featured of this.selectedProducts) {
+	  output.push(products[featured]);
+	}
+      }
+      return output;
+    }
+  }
+};
